39024c7c80f881918333596d00dc3d8946838b27
Jorell Jusay <jojusay@ucsd.edu>
2026-05-02 23:26:56 -0700

    added event listener to change image and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,30 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let audio_choice = document.getElementById("horn-select");
+  let audio = document.querySelector("audio");
+  let image = document.querySelector("#volume-controls img");
+
+  //Change sound of 
+  audio_choice.addEventListener("change sound", function() {
+    if (audio_choice.value == "air-horn") {
+      audio.src = "assets/audio/air-horn.mp3";
+    } else if (audio_choice.value == "car-horn") {
+      audio.src = "assets/audio/car-horn.mp3";
+    } else if (audio_choice.value == "party-horn") {
+      audio.src = "assets/audio/party-horn.mp3";
+    }
+    console.log("audio changed to " + audio_choice.value);
+  });
+
+  audio_choice.addEventListener("change image", function() {
+    if (audio_choice.value == "air-horn") {
+      audio.src = "assets/images/air-horn.svg";
+    } else if (audio_choice.value == "car-horn") {
+      audio.src = "assets/images/car-horn.svg";
+    } else if (audio_choice.value == "party-horn") {
+      audio.src = "assets/images/party-horn.svg";
+    }
+    console.log("image changed to " + audio_choice.value);
+  });
 }
